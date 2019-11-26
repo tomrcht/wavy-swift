219dd560f10e8e1cc55550bcb02653fb00db00b0
Tom Rochat <tom.roch4t@gmail.com>
2019-11-26 21:18:56 +0100

    add spinner

--- Wavyswift/Controllers/HomeViewController.swift
+++ Wavyswift/Controllers/HomeViewController.swift
@@ -12,13 +12,19 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var wavyButton: UIButton!
 
+    private var spinner = UIActivityIndicatorView()
     private var quoteManager = QuoteManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        spinner.color = .red
+        spinner.center = CGPoint(x: quoteLabel.frame.size.width / 2, y: quoteLabel.frame.size.height / 2)
     }
 
     @IBAction func getQuote(_ sender: Any) {
+        prepareForQuery()
+
         quoteManager.fetchQuote { (result) in
             switch result {
             case .success(let quote):
@@ -32,11 +38,26 @@ class HomeViewController: UIViewController {
     private func setNewQuote(quote: Quote) {
         DispatchQueue.main.async {
             self.quoteLabel.text = quote.quote
+            self.didTerminateQuery()
         }
     }
 
     private func didReceiveError(error: Error) {
         print(error)
+        didTerminateQuery()
+    }
+}
+
+extension HomeViewController {
+    private func prepareForQuery() {
+        spinner.startAnimating()
+        quoteLabel.text = ""
+        quoteLabel.addSubview(spinner)
+    }
+
+    private func didTerminateQuery() {
+        spinner.stopAnimating()
+        spinner.removeFromSuperview()
     }
 }
 
