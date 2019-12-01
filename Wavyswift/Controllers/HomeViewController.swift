f6a02012a327448d15f76822b85310ac348df832
Tom Rochat <tom.roch4t@gmail.com>
2019-12-01 23:38:23 +0100

    you can move kanye's head up and down

--- Wavyswift/Controllers/HomeViewController.swift
+++ Wavyswift/Controllers/HomeViewController.swift
@@ -11,15 +11,33 @@ import UIKit
 class HomeViewController: UIViewController {
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var wavyButton: UIButton!
-
+    @IBOutlet weak var mainLogo: UIImageView!
+    
     private var spinner = UIActivityIndicatorView()
     private var quoteManager = QuoteManager()
+
+    private var initialLogoPosition: CGRect!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         spinner.color = UIColor(named: "TextColor")
         spinner.center = CGPoint(x: quoteLabel.frame.size.width / 2, y: quoteLabel.frame.size.height / 2)
+
+        initialLogoPosition = mainLogo.frame
+        mainLogo.isUserInteractionEnabled = true
+
+        let upGesture = UISwipeGestureRecognizer(target: self, action: #selector(self.onLogoSwipeUp))
+        upGesture.direction = .up
+        mainLogo.addGestureRecognizer(upGesture)
+
+        let downGesture = UISwipeGestureRecognizer(target: self, action: #selector(self.onLogoSwipeDown))
+        downGesture.direction = .down
+        mainLogo.addGestureRecognizer(downGesture)
+
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(self.onDoubleTap))
+        doubleTap.numberOfTapsRequired = 2
+        mainLogo.addGestureRecognizer(doubleTap)
     }
 
     @IBAction func getQuote(_ sender: Any) {
@@ -51,7 +69,7 @@ class HomeViewController: UIViewController {
     }
 }
 
-extension HomeViewController {
+private extension HomeViewController {
     private func prepareForQuery() {
         wavyButton.isEnabled = false
 
@@ -68,3 +86,32 @@ extension HomeViewController {
     }
 }
 
+private extension HomeViewController {
+    @objc func onLogoSwipeUp(_ sender: UISwipeGestureRecognizer) -> Void {
+        let x = mainLogo.frame.origin.x
+        let y = mainLogo.frame.origin.y
+
+        guard y - 50 >= 0 else {
+            return
+        }
+
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+            self.mainLogo.frame = CGRect(x: x, y: y - 50, width: self.mainLogo.frame.width, height: self.mainLogo.frame.height)
+        }, completion: nil)
+    }
+
+    @objc func onLogoSwipeDown(_ sender: UISwipeGestureRecognizer) -> Void {
+        let x = mainLogo.frame.origin.x
+        let y = mainLogo.frame.origin.y
+
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+            self.mainLogo.frame = CGRect(x: x, y: y + 50, width: self.mainLogo.frame.width, height: self.mainLogo.frame.height)
+        }, completion: nil)
+    }
+
+    @objc func onDoubleTap(_ sender: UITapGestureRecognizer) -> Void {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+            self.mainLogo.frame = self.initialLogoPosition
+        }, completion: nil)
+    }
+}
